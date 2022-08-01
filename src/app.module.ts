4b8528268304ade59698e88680574e35ed94c359
Sakura404 <380487608@qq.com>
2022-08-01 18:15:15 +0800

    配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
 import { FriendModule } from './friend/friend.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import envConfig from '../config/env';
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      logging: ['error'],
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: 3306,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE,
-      entities: ['dist/**/entities/*.entity{.ts,.js}'],
-      synchronize: true,
-      entityPrefix: 'nc_',
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: [envConfig.path] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        logging: ['error'],
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: 3306,
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_DATABASE'),
+        entities: ['dist/**/entities/*.entity{.ts,.js}'],
+        synchronize: true,
+        entityPrefix: 'nc_',
+      }),
     }),
     UserModule,
     GroupModule,
